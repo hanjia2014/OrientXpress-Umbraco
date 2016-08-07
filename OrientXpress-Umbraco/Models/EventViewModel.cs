b673af47c784be3cdd2c9d37ac4ae9c5ec81c793
Datacom <Datacom@CALLUMG-LT002>
2016-08-08 10:02:20 +1200

    remove ng2

--- OrientXpress-Umbraco/Models/EventViewModel.cs
+++ OrientXpress-Umbraco/Models/EventViewModel.cs
@@ -7,5 +7,18 @@ namespace OrientXpress_Umbraco.Models
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime Date { get; set; }
+
+        public string TruncateContent
+        {
+            get
+            {
+                var maxLength = 100;
+                //trim the string to the maximum length
+                var trimmedString = Content.Substring(0, maxLength);
+                //re-trim if we are in the middle of a word
+                trimmedString = trimmedString.Substring(0, Math.Min(trimmedString.Length, trimmedString.LastIndexOf(" ")));
+                return trimmedString + "...";
+            }
+        }
     }
 }
